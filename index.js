92acae538b3e1dd2711b2198f5432592008190a0
GabrielLaxy <gablaxycarreira@gmail.com>
2023-11-29 19:16:18 -0300

    correção do index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 
-const appid=`ef0b0973b783e0614ac87612ec04344b`
-const cidade=`Rome`
-const pais=`IT`
-const cnt =`1`
+const appid=`ef0b0973b783e0614ac87612ec04344b`;
+const cidade=`Rome`;
+const units = "metric";
+const lang = "pt_BR"
+const pais=`IT`;
+const cnt =`1`;
 
 const GeoCoding = `http://api.openweathermap.org/geo/1.0/direct?q=${cidade},${pais}&limit=${cnt}&appid=${appid}`;
 
@@ -29,7 +31,7 @@ const clima_atual = async function() {
     const coordenadas2 = (await axios.get(GeoCoding)).data;
     let latitude2 = coordenadas2[0]['lat'];
     let longitude2 = coordenadas2[0]['lon'];
-    const Current = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude2}&lon=${longitude2}&appid=${APPID}`;
+    const Current = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude2}&lon=${longitude2}&units=${units}}&lang=${lang}&appid=${appid}`;
     const temperatura = (await axios.get(Current)).data;
     sensacao_term = temperatura.main.feels_like;
     desc = temperatura.weather[0].description;
